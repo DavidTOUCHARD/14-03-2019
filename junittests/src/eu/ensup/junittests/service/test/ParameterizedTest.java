1518e05535679209f5545147a2bec107fec741ac
David TOUCHARD <david.touchard@hotmail.fr>
2019-03-13 16:41:42 +0100

    Parameterized test

--- junittests/src/eu/ensup/junittests/service/test/ParameterizedTest.java
+++ junittests/src/eu/ensup/junittests/service/test/ParameterizedTest.java
@@ -3,12 +3,37 @@ package eu.ensup.junittests.service.test;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import eu.ensup.junittests.service.Parameter;
+import java.util.Arrays;
+import java.util.Collection;
 
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+@RunWith(Parameterized.class)
 public class ParameterizedTest {
+    @Parameters
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][] {     
+                 { 0, 0 }, { 1, 1 }, { 2, 1 }, { 3, 2 }, { 4, 3 }, { 5, 5 }, { 6, 8 }  
+           });
+    }
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
-	}
+    private int fInput;
 
+    private int fExpected;
+
+    private Parameter parameterized = new Parameter();
+    
+    public ParameterizedTest(int input, int expected) {
+        this.fInput = input;
+        this.fExpected = expected;
+    }
+
+    @Test
+    public void test() {
+        assertEquals(fExpected, parameterized.compute(fInput));
+    }
 }
